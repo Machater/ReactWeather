d599719604da8770068f6f79580ff33b86d2a6bc
Brian Cohen <brian.cohen0356@gmail.com>
2016-09-29 15:31:25 -0500

    style about page and add tools used to build

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,13 +1,24 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = (props) => {
-  return (
-    <div>
-    <h3>About</h3>
-    <p>Welcome to the about page</p>
-    </div>
-  );
+    return (
+        <div>
+            <h1 className="text-center">About</h1>
+            <p>Tools used to build this weather app</p>
+            <ul>
+                <li>Built with React</li>
+                <li>Version control with git and GitHub</li>
+                <li>Deployed to Heroku</li>
+                <li>Styled with Foundation</li>
+                <li>Bundled with Webpack</li>
+                <li>Running on Express for Node.js</li>
+                <li>Compiled with Babel</li>
+                <li>Packages managed with npm</li>
+                <li>Weather API from OpenWeatherMap</li>
+            </ul>
+        </div>
+    );
 };
-
 
 module.exports = About;
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,20 +2,20 @@ var React = require('react');
 var {Link} = require('react-router');
 
 var Examples = (props) => {
-  return (
-    <div>
-    <h1 className="text-center">Examples</h1>
-    <p>Here are a few example locations to try out:</p>
-    <ol>
-    <li>
-    <Link to='/?location=Austin'>Austin, TX</Link>
-    </li>
-    <li>
-    <Link to='/?location=Zagreb'>Zagreb, Croatia</Link>
-    </li>
-    </ol>
-    </div>
-  );
+    return (
+        <div>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few example locations to try out:</p>
+            <ol>
+                <li>
+                    <Link to='/?location=Austin'>Austin, TX</Link>
+                </li>
+                <li>
+                    <Link to='/?location=Zagreb'>Zagreb, Croatia</Link>
+                </li>
+            </ol>
+        </div>
+    );
 };
 
 module.exports = Examples;
